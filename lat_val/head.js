374964c5095541f949f2c955e0ad88ccdd11ef9b
officialErnests <ernestspursw@gmail.com>
2024-05-13 17:00:20 +0000

    yes

---
+++ lat_val/head.js
@@ -0,0 +1,13 @@
+function analize() {
+    
+}
+
+window.onload = function test(){
+    const input = console.log(document.getElementById("input"))
+    const selected = console.log(document.getElementById("selected"))
+    const suggested = console.log(document.getElementById("suggested"))
+}
+/**  
+while (true) {
+
+}*/
